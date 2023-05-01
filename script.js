c4e4f2983c35d225b176c5670bd3387e9f5d7944
host-glitch <64912959+host-glitch@users.noreply.github.com>
2023-05-01 15:22:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+const itemsContainer = document.querySelector('#items');
+const categoriesContainer = document.querySelector('#categories');
+const allBtn = document.querySelector('#all');
+const totalItems = document.querySelector('#total-items');
+
+console.log('Before fetch');
+
+// list of allowed categories
+const allowedCategories = ['Warframes', 'Melee', 'Primary', 'Secondary', 'Pets', 'Arch-Gun', 'Arch-Melee', 'Archwing', 'Sentinels', 'SentinelWeapons'];
+
+fetch('http://localhost:3000/api/items')
+  .then(response => response.json())
+  .then(items => {
+    console.log('Items:', items);
+
+    const categories = getCategories(items);
+
+    // filter categories to only show allowed categories
+    const allowedCategoriesHTML = categories.filter(category => allowedCategories.includes(category)).map(category => `
+      <button id="${category}" class="category-btn" data-category="${category}">
+        ${category}
+      </button>
+    `).join('');
+
+    categoriesContainer.innerHTML = `
+      <button id="all" class="category-btn" data-category="all">
+        All
+      </button>
+      ${allowedCategoriesHTML}
+    `;
+
+    const categoryBtns = categoriesContainer.querySelectorAll('.category-btn');
+
+    categoryBtns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        const category = btn.getAttribute('data-category');
+
+        // filter items to only show items from allowed categories
+        const filteredItems = category === 'all' ? items.filter(item => allowedCategories.includes(item.category)) : items.filter(item => {
+          return item.category === category && allowedCategories.includes(item.category);
+        });
+
+        const itemsHTML = filteredItems.map(item => `
+          <div>
+            <label for="${item.uniqueName}"><h3>${item.name}</h3></label>
+			<label for="built">Built:</label> 
+			<input type="checkbox" id="${item.uniqueName} name="built">
+           
+			<p>${item.description}</p>
+			<p>Build price: ${item.buildPrice} credits</p>
+          </div>
+        `).join('');
+
+        itemsContainer.innerHTML = itemsHTML;
+        
+        // update total items count
+        totalItems.textContent = `Total Items: ${filteredItems.length}`;
+      });
+    });
+
+    allBtn.addEventListener('click', () => {
+      // filter items to only show items from allowed categories
+      const filteredItems = items.filter(item => allowedCategories.includes(item.category));
+
+      const itemsHTML = filteredItems.map(item => `
+        <div>
+          <input type="checkbox" id="${item.uniqueName}">
+          <label for="${item.uniqueName}">${item.name}</label>
+		  <p>Description: ${item.description}</p>
+        </div>
+      `).join('');
+
+      itemsContainer.innerHTML = itemsHTML;
+      
+      // update total items count
+      totalItems.textContent = `Total Items: ${filteredItems.length}`;
+    });
+  })
+  .catch(error => console.error(error));
+
+console.log('After fetch');
+
+function getCategories(items) {
+  const categories = new Set();
+  items.forEach(item => {
+    categories.add(item.category);
+  });
+  return Array.from(categories);
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+app.use(cors());
+
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+app.get('/api/items', async (req, res) => {
+  try {
+   const response = await axios.get('https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/All.json');
+     console.log(response.data); // Add this line to log the response data
+	res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching data from GitHub');
+  }
+});
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
